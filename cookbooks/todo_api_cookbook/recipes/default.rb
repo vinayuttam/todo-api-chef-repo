297ebe41327517671b5b8d38d1c8750bd80cbb6a
Vinay Uttam Vemparala <vinayuttamvemparala@live.com>
2018-09-15 15:11:19 +0530

    Add file

--- cookbooks/todo_api_cookbook/recipes/default.rb
+++ cookbooks/todo_api_cookbook/recipes/default.rb
@@ -6,9 +6,17 @@
 
 cerner_tomcat 'todo_api' do
   install_java false
-  version '9.0.1'
+  version node['todo_api']['tomcat']['version']
 
   web_app 'ROOT' do
     source 'https://s3.ap-south-1.amazonaws.com/todos-api/todos-api.war'
+
+    cookbook_file 'WEB-INF/classes/database.properties' do
+      source 'test/testFile'
+    end
   end
+
+  # web_app 'todos-api' do
+  #   source 'https://s3.ap-south-1.amazonaws.com/todos-api/todos-api.war'
+  # end
 end
