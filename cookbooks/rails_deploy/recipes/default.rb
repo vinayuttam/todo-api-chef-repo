227e851a25004f12266386d20c0a2bda3a45d438
Vinay Uttam Vemparala <15381417+vinayuttam@users.noreply.github.com>
2019-12-18 01:03:05 +0530

    steps to install ruby

--- cookbooks/rails_deploy/recipes/default.rb
+++ cookbooks/rails_deploy/recipes/default.rb
@@ -4,7 +4,14 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
-file '/home/ubuntu/text.text' do
-  content 'test contents'
-  mode '0755'
+execute 'Configure GPG key for RVM' do
+  command 'gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB'
+end
+
+execute 'Install RVM' do
+  command '\curl -sSL https://get.rvm.io | bash -s stable'
+end
+
+execute 'Install Ruby' do
+  command 'rvm install ruby-2.6.3'
 end
